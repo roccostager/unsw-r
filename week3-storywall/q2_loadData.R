4879406d394f23f2d0a88e9c6e445fc4679ab536
rocco <99860439+roccostager@users.noreply.github.com>
2026-06-20 13:15:17 +1000

    rename stuff

--- week3-storywall/q2_loadData.R
+++ week3-storywall/q2_loadData.R
@@ -2,3 +2,4 @@ setwd("C:/Users/rocco/dev/repos/unsw-r/week3-storywall")
 rm(list = ls())
 
 airbnb.df <- read.csv("data/AirbnbSydney.csv")
+str(airbnb.df)
--- week3-storywall/q5_approximateIncome.R
+++ week3-storywall/q5_approximateIncome.R
@@ -5,8 +5,8 @@ reviewRatio <- 0.7  # 70% of people leave reviews
 avgNightsPerStay <- 3  # 3-night average stay
 
 # Estimate Number of Nights in a Month
-stays <- airbnb.df$reviews_per_month / 0.7
-nights <- pmin(pmax(airbnb.df$minimum_nights, avgNightsPerStay), airbnb.df$maximum_nights) * avgNightsPerStay
+stays <- airbnb.df$reviews_per_month / reviewRatio
+nights <- pmin(pmax(airbnb.df$minimum_nights, avgNightsPerStay), airbnb.df$maximum_nights) * stays
 
 # Sanity check: use availability_30 as cap
 nights <- pmin(nights, 30 - airbnb.df$availability_30)
